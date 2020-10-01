5d5d0b3e1dd80cdf79b41ef1314d07b8ec2d362d
Billzg13 <Billzg1234@gmail.com>
2020-10-01 15:04:34 +0300

    add json in quick search

--- src/controllers/quick_search_controller.py
+++ src/controllers/quick_search_controller.py
@@ -7,6 +7,11 @@ logger = logging.getLogger(__name__)
 
 class quick_search_controller(Resource):
     def post(self):
-        
+        print('in quick search')
+        json_data = request.get_json(force=True)
+        print(json_data)
+        user = json_data['user']
+        print(user['types'])
+
         logger.debug("In action: post quick search controller")
         return { "message":"hello!" }
